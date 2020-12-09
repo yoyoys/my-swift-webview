9b7b78519fa74faa10074a1fa0b043b64c357f42
Yoyo Young <yoyoys@livemail.tw>
2020-12-09 16:44:28 +0800

    Initial Commit

---
+++ mywebview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mywebview
+//
+//  Created by Yoyo Young on 2020/12/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mywebview/mywebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  mywebviewApp.swift
+//  mywebview
+//
+//  Created by Yoyo Young on 2020/12/9.
+//
+
+import SwiftUI
+
+@main
+struct mywebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
